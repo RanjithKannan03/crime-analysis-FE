61058718063e15650b38695f43e4e9f7a2a69c3c
Navaneetha Krishnan K S <navaneethan8max@icloud.com>
2024-04-13 23:26:11 +0530

    pdf endpoint created

--- src/pages/DataGrid/Pdf.jsx
+++ src/pages/DataGrid/Pdf.jsx
@@ -1,31 +1,35 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from "react";
 import MaterialReactTable from "material-react-table";
-import { userData } from '../../data';
-import './DataGrid.css'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { userData } from "../../data";
+import "./DataGrid.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Pdf = () => {
+    const [data, setData] = useState(null);
+    
+  const theme = useMemo(() =>
+    createTheme({
+      palette: {
+        mode: "dark",
+      },
+    })
+  );
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
    
-    const theme = useMemo(
-        () => createTheme({
-            palette: {
-                mode: "dark"
-            }
-        })
-    )
+    setData(e.target.files[0]);
+    console.log(file);
+   
+  };
+  return (
+    <div className="table-container">
+      <ThemeProvider theme={theme}>
+        <input type="file" onChange={handleFileChange} />
+        <div className="text-yellow-400">Please Upload the PDF file !</div>
+      </ThemeProvider>
+    </div>
+  );
+};
 
-    return (
-        <div className="table-container">
-            <ThemeProvider theme={theme}>
-                    <input type="file" accept=".pdf"/>
-                   
-            </ThemeProvider>
-            <div className='text-yellow-400'>
-                helo navane
-            </div>
-        </div>
-    )
-}
-
-export default Pdf
+export default Pdf;
