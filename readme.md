1614e548cd136bc97796b66866124e6ce06a3fd7
Navaneetha Krishnan K S <navaneethan8max@icloud.com>
2024-05-21 22:35:14 +0530

    first commit

--- readme.md
+++ readme.md
@@ -35,3 +35,4 @@ Screenshots<br>
 
 
 # crime-analysis-FE
+# crime-analysis-FE
--- src/pages/DataGrid/ImageGen.jsx
+++ src/pages/DataGrid/ImageGen.jsx
@@ -38,6 +38,7 @@ const Pdf = () => {
       setIsLoading(false);
     } catch (e) {
       console.log(e);
+     
     }
   }
 
